2bbbd20082b58d82d2a72224adee6d5922f19205
Jan <jan@janwilts.com>
2017-12-19 11:58:31 +0100

    Parser selection groundwork

--- bigmovie/parser/src/main/java/com/janwilts/bigmovie/parser/parsers/Parser.java
+++ bigmovie/parser/src/main/java/com/janwilts/bigmovie/parser/parsers/Parser.java
@@ -1,20 +1,51 @@
 package com.janwilts.bigmovie.parser.parsers;
 
+import com.janwilts.bigmovie.parser.enums.RequiredFile;
+
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class Parser {
     private static Parser currentParser;
 
+    private File file;
+
+    public List<RequiredFile> requiredFiles;
+
     public static void parseFile(File file) {
-        // TODO Implement parser selection logic here
-        currentParser = new MovieParser(file);
+        // TODO: Add corresponding parsers
+        if(checkFileName(file, 0))
+            currentParser = new MovieParser(file);
+        else if(checkFileName(file, 1))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 2))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 3))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 4))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 5))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 6))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 7))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 8))
+            throw new UnsupportedOperationException();
+        else if(checkFileName(file, 9))
+            throw new UnsupportedOperationException();
+
         currentParser.parse();
     }
 
-    private File file;
+    private static Boolean checkFileName(File file, int index) {
+        return file.getName().equals(RequiredFile.getList().get(index));
+    }
 
     public Parser(File file) {
         this.file = file;
+        this.requiredFiles = new ArrayList<>();
     }
 
     public abstract void parse();
