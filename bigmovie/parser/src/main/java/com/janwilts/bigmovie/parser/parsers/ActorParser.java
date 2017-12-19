25c9c81d9a48c66039805f6c331f5998f1779621
Y Kooistra <yannick.kooistra@gmail.com>
2017-12-19 14:47:31 +0100

    ActorParser fully functional

--- bigmovie/parser/src/main/java/com/janwilts/bigmovie/parser/parsers/ActorParser.java
+++ bigmovie/parser/src/main/java/com/janwilts/bigmovie/parser/parsers/ActorParser.java
@@ -68,9 +68,9 @@ public class ActorParser extends Parser{
                         if(searchLine.contains("(") && Character.isDigit(searchLine.charAt(leftCommaInd + 1)) &&
                                 (rightCommaInd - leftCommaInd == 5 && Character.isDigit(searchLine.charAt(leftCommaInd + 1)) &&
                                         Character.isDigit(searchLine.charAt(rightCommaInd - 1))) ||
-                                rightCommaInd - leftCommaInd > 5 && Character.isDigit(searchLine.charAt(leftCommaInd + 1)) &&
+                                (rightCommaInd - leftCommaInd > 5 && Character.isDigit(searchLine.charAt(leftCommaInd + 1)) &&
                                         searchLine.charAt(rightCommaInd - 1) == 'I' || searchLine.charAt(rightCommaInd - 1) == 'V' ||
-                                        searchLine.charAt(rightCommaInd - 1) == 'X')
+                                        searchLine.charAt(rightCommaInd - 1) == 'X'))
                         {
 
                             currentFilm = line.substring(line.indexOf("\t"), line.indexOf("(", line.indexOf(searchLine))).trim();
