89c73ec3b65e29a096c1d427fef5a827f5af2143
Wexalian <larsgerrits99@gmail.com>
2018-01-10 11:23:20 +0100

    Initial steps of rivescript handling

--- bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/Main.java
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/Main.java
@@ -1,7 +1,16 @@
 package com.janwilts.bigmovie.chatbot;
 
+import com.rivescript.Config;
+import com.rivescript.RiveScript;
+
 public class Main {
     public static void main(String[] args) {
-
+        RiveScript bot = new RiveScript(Config.utf8());
+        bot.loadDirectory("src/main/resources/rivescript");
+        bot.sortReplies();
+        
+        String response = bot.reply("user", "hello bot");
+        System.out.println(response);
+        
     }
 }
