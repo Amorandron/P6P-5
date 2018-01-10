662dbae71438d68694fecaee00ab998fea65be8d
Wexalian <larsgerrits99@gmail.com>
2018-01-10 12:53:00 +0100

    Only respspond to mentions

--- bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/discord/MessageListener.java
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/discord/MessageListener.java
@@ -14,8 +14,9 @@ public class MessageListener implements IListener<MessageReceivedEvent> {
     @Override
     public void handle(MessageReceivedEvent event) {
         String input = event.getMessage().getContent();
-        if (input.startsWith("!")) {
-            String message = input.substring(1,input.length());
+        if (event.getMessage().getMentions().size() == 1 && event.getMessage().getMentions().contains(event.getClient().getOurUser())) {
+            
+            String message = input.substring(input.indexOf(">") + 1, input.length());
             String response = bot.reply(event.getAuthor().getDisplayName(event.getGuild()), message);
             event.getChannel().sendMessage(response);
         }
