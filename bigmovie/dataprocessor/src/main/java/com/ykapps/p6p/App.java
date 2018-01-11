776bb1d6e013df6c553adda591c045b8aa13436c
Yannick Kooistra <yannick.kooistra@gmail.com>
2018-01-11 11:31:58 +0100

    Actor parser now handles V/VG/TV

--- bigmovie/dataprocessor/src/main/java/com/ykapps/p6p/App.java
+++ bigmovie/dataprocessor/src/main/java/com/ykapps/p6p/App.java
@@ -39,7 +39,7 @@ public class App extends Jooby {
         onStart(() -> {
             model = new Model(require(Database.class));
 
-            re=new Rengine();
+            re = new Rengine();
             // the engine creates R is a new thread, so we should wait until it's ready
             if (!re.waitForR()) {
                 throw new Exception("Cannot load R");
