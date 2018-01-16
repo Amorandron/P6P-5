15801ac0461319f138253a020d822696323a9348
Yannick Kooistra <yannick.kooistra@gmail.com>
2018-01-16 10:26:45 +0100

    Small actor parser fix

--- bigmovie/parser/src/main/java/com/janwilts/bigmovie/parser/inserters/Inserter.java
+++ bigmovie/parser/src/main/java/com/janwilts/bigmovie/parser/inserters/Inserter.java
@@ -20,9 +20,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public abstract class Inserter {
-    //private static final String[] order = new String[] {"movies", "countries", "genres", "soundtracks", "actors"};
-    private static final String[] order = new String[] {"movies", "actors"};
-
+    private static final String[] order = new String[] {"movies", "countries", "genres", "soundtracks", "actors"};
     protected DatabaseConnection connection;
     protected File csv;
 
