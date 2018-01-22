86e13ead859cdc335c8b3a49609da642180b3dd9
svenmark <svenmark10@gmail.com>
2018-01-22 14:25:36 +0100

    Add ActorInfoSubroutine Add MovieInfoSubroutine

---
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorInfoSubroutine.java
@@ -0,0 +1,16 @@
+package com.janwilts.bigmovie.chatbot.subroutines;
+
+import com.janwilts.bigmovie.chatbot.discord.DiscordBot;
+import com.rivescript.RiveScript;
+
+public class ActorInfoSubroutine extends Routine {
+
+    public ActorInfoSubroutine(DiscordBot bot) {super (bot);}
+
+    @Override
+    public String call(RiveScript rs, String[] args) {
+        String result = "";
+
+        return result;
+    }
+}
---
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/MovieInfoSubroutine.java
@@ -0,0 +1,16 @@
+package com.janwilts.bigmovie.chatbot.subroutines;
+
+import com.janwilts.bigmovie.chatbot.discord.DiscordBot;
+import com.rivescript.RiveScript;
+
+public class MovieInfoSubroutine extends Routine {
+
+    public MovieInfoSubroutine(DiscordBot bot) { super(bot); }
+
+    @Override
+    public String call(RiveScript rs, String[] args) {
+        String result = "";
+
+        return result;
+    }
+}
