aba6dcf6649360c04da45ce10f52cc474ab7d14d
svenmark <svenmark10@gmail.com>
2018-01-24 12:13:39 +0100

    Small fix

--- bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorRoleSubroutine.java
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorRoleSubroutine.java
@@ -17,45 +17,53 @@ public class ActorRoleSubroutine extends Routine {
     @Override
     public String call(RiveScript rs, String[] args) {
         StringBuilder result = new StringBuilder();
+        StringBuilder firstname = new StringBuilder();
+        StringBuilder lastname = new StringBuilder();
         StringBuilder search = new StringBuilder();
         if (args.length <= 0) {
             return "Send args";
         }
 
-        if (args[0].contains("actor")) {
+        if (args[0].contains("actor") && (args.length > 1)) {
             // Search for movies with actor x
 
             List<Movie> api = null;
 
             APIRequester requester = new APIRequester(Movie.class);
-
-            for(int i = 1; i < args.length; i++) {
-                search.append(args[i]);
-                search.append(" ");
+            if(args.length > 2) {
+                //Add second name
+                for (int i = 2; i < args.length; i++) {
+                    lastname.append(args[i]);
+                    if (args.length - 1 != i) {
+                        lastname.append(" ");
+                    }
+                }
             }
+            // Add firstname
+            firstname.append(args[1]);
 
-            result = search;
-
-//            try {
-//                api = requester.getArrayFromAPI("/q/d2/?actor=" + search);
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            for(int i = 0; i < api.size(); i++) {
-//                focusedMovies.put(i + 1, api.get(i));
-//            }
-//            result.append(PrintUtils.movieListPrint(focusedMovies));
+            try {
+                api = requester.getArrayFromAPI(String.format("/q/d2/?lastname=%s&firstname=%s", lastname, firstname));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            for(int i = 0; i < api.size(); i++) {
+                focusedMovies.put(i + 1, api.get(i));
+            }
+            result.append(PrintUtils.movieListPrint(focusedMovies));
         }
-        else {
+        else if (args[0].contains("movie") && (args.length > 1)) {
             // Give actors who play in movie x
 
             List<Actor> api = null;
 
             APIRequester requester = new APIRequester(Actor.class);
 
-            for(int i = 1; i < args.length; i++) {
-                search.append(args[i]);
+            search.append(args[1]);
+            for(int i = 2; i < args.length; i++) {
                 search.append(" ");
+                search.append(args[i]);
+
             }
 //            try {
 //                api = requester.getArrayFromAPI("/q/");
@@ -66,8 +74,9 @@ public class ActorRoleSubroutine extends Routine {
 //                focusedActors.put(i + 1, api.get(i));
 //            }
 //            result.append(PrintUtils.actorListPrint(focusedActors));
-
-            result = search;
+        }
+        else {
+            result.append("I didn't understand");
         }
 
 
