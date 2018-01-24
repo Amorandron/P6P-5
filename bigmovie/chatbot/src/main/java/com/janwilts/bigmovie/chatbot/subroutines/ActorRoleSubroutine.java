0d479e09eae587c7a2386c230b2a3504a4bd9348
svenmark <svenmark10@gmail.com>
2018-01-24 11:22:16 +0100

    Question D2 subroutine, both ways movie > actors, actor > movies

--- bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorRoleSubroutine.java
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorRoleSubroutine.java
@@ -1,7 +1,13 @@
 package com.janwilts.bigmovie.chatbot.subroutines;
 
 import com.janwilts.bigmovie.chatbot.discord.DiscordBot;
+import com.janwilts.bigmovie.chatbot.models.Actor;
+import com.janwilts.bigmovie.chatbot.models.Movie;
+import com.janwilts.bigmovie.chatbot.util.APIRequester;
+import com.janwilts.bigmovie.chatbot.util.PrintUtils;
 import com.rivescript.RiveScript;
+
+import java.util.List;
 
 public class ActorRoleSubroutine extends Routine {
     public ActorRoleSubroutine(DiscordBot bot) {
@@ -10,32 +16,62 @@ public class ActorRoleSubroutine extends Routine {
 
     @Override
     public String call(RiveScript rs, String[] args) {
-        String result = "";
-
-        //TODO get request from api.
-
-        if (args.length == 0) {
-            return result;
+        StringBuilder result = new StringBuilder();
+        StringBuilder search = new StringBuilder();
+        if (args.length <= 0) {
+            return "Send args";
         }
 
-        if (args.length == 3) {
-            result += args[0] + " " + args[1] + ", " + args[2];
-        }
-        else if (args.length < 3) {
-            result += args[0] + ", " + args[1];
+        if (args[0].contains("actor")) {
+            // Search for movies with actor x
+
+            List<Movie> api = null;
+
+            APIRequester requester = new APIRequester(Movie.class);
+
+            for(int i = 1; i < args.length; i++) {
+                search.append(args[i]);
+                search.append(" ");
+            }
+
+            result = search;
+
+//            try {
+//                api = requester.getArrayFromAPI("/q/d2/?actor=" + search);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            for(int i = 0; i < api.size(); i++) {
+//                focusedMovies.put(i + 1, api.get(i));
+//            }
+//            result.append(PrintUtils.movieListPrint(focusedMovies));
         }
         else {
-            String firstName = args[0];
-            String lastName = "";
-            for (int i = 1; i<args.length - 1; i++) {
-                lastName += args[i];
-                lastName += " ";
-            }
-            String type = args[args.length - 1];
+            // Give actors who play in movie x
 
-            result = firstName + " " + lastName + " " + type;
+            List<Actor> api = null;
+
+            APIRequester requester = new APIRequester(Actor.class);
+
+            for(int i = 1; i < args.length; i++) {
+                search.append(args[i]);
+                search.append(" ");
+            }
+//            try {
+//                api = requester.getArrayFromAPI("/q/");
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            for(int i = 0; i < api.size(); i++) {
+//                focusedActors.put(i + 1, api.get(i));
+//            }
+//            result.append(PrintUtils.actorListPrint(focusedActors));
+
+            result = search;
         }
 
-        return result;
+
+
+        return result.toString();
     }
 }
