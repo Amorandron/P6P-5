84de98b99960b1bb60c8c9b746a0eef2d88bbce1
svenmark <svenmark10@gmail.com>
2018-01-24 12:14:34 +0100

    Add reversefor SQL_D2

--- bigmovie/dataprocessor/src/main/java/com/ykapps/bigmovie/models/Model.java
+++ bigmovie/dataprocessor/src/main/java/com/ykapps/bigmovie/models/Model.java
@@ -29,6 +29,7 @@ public class Model {
     public static final String SQL_D1_MOST_DAYS = "SELECT * FROM gross WHERE gross_id IN (SELECT gross_id FROM gross_by_period WHERE transaction_date_delta = ?) ORDER BY amount DESC LIMIT 10";
     public static final String SQL_D1_LEAST_DAYS = "SELECT * FROM gross WHERE gross_id IN (SELECT gross_id FROM gross_by_period WHERE transaction_date_delta = ?) ORDER BY amount ASC LIMIT 10";
     public static final String SQL_D2 = "SELECT * FROM public.movie WHERE movie_id IN (SELECT movie_id FROM public.movie_actor AS ma, public.actor AS a WHERE ma.actor_id = a.actor_id AND a.name = ?) ORDER BY release_year";
+    public static final String SQL_D2_REVERSE = "SELECT * FROM public.actor WHERE actor_id IN( SELECT ma.actor_id FROM public.movie_actor AS ma, public.actor AS a, public.movie AS mo WHERE ma.actor_id = a.actor_id AND ma.movie_id = mo.movie_id AND mo.title = 'Insurgent' ) ORDER BY name";
     public static final String SQL_B4 = "SELECT * " +
             "FROM public.country " +
             "WHERE lower(country) LIKE ?";
