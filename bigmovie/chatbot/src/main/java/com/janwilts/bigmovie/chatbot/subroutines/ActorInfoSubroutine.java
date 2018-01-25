d1e994726696a58773cf9fe0992ef7fa147732fe
svenmark <svenmark10@gmail.com>
2018-01-25 13:45:52 +0100

    Remove country from the options in basicQuestions Make ActorInfoSub compatible with backend naming

--- bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorInfoSubroutine.java
+++ bigmovie/chatbot/src/main/java/com/janwilts/bigmovie/chatbot/subroutines/ActorInfoSubroutine.java
@@ -28,11 +28,11 @@ public class ActorInfoSubroutine extends Routine {
 
         APIRequester requester = new APIRequester(Actor.class);
 
-        if(args.length > 2) {
+        if(args.length > 1) {
             //Add second name
-            for (int i = 1; i < args.length - 1; i++) {
+            for (int i = 0; i < args.length - 1; i++) {
                 firstname.append(args[i].toLowerCase());
-                if (args.length - 2 != i) {
+                if (args.length - 1 != i) {
                     firstname.append(" ");
                 }
             }
