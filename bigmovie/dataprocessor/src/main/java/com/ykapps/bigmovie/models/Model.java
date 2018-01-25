b1b9b1c1f3acbeb1356b8518f6aa90883323d4a0
Amorandron <jan@janwilts.com>
2018-01-25 15:32:46 +0100

    Format SQL and commenting

--- bigmovie/dataprocessor/src/main/java/com/ykapps/bigmovie/models/Model.java
+++ bigmovie/dataprocessor/src/main/java/com/ykapps/bigmovie/models/Model.java
@@ -13,26 +13,164 @@ public class Model {
         GROSS,
         COUNTRY,
     }
-    public static final String SQL_SELECT_MOVIE = "SELECT * FROM movie WHERE movie_id=?";
-    public static final String SQL_A7_MOST = "SELECT * FROM movie WHERE budget IS NOT NULL ORDER BY budget DESC LIMIT 5";
-    public static final String SQL_A7_LEAST = "SELECT * FROM movie WHERE budget IS NOT NULL ORDER BY budget ASC LIMIT 5";
-    public static final String SQL_A8_EVER = "SELECT cur.* FROM gross cur WHERE NOT EXISTS (SELECT * FROM gross high WHERE high.movie_id = cur.movie_id AND high.country_id = cur.country_id AND high.amount > cur.amount) AND cur.amount IS NOT NULL AND cur.country_id = (SELECT country_id FROM country WHERE LOWER(country) = ? LIMIT 1) ORDER BY cur.amount DESC LIMIT 20";
-    public static final String SQL_A8_DAYS_P1 = "SELECT * FROM gross WHERE gross_id IN (SELECT gross_id FROM gross_by_period WHERE country_id = (SELECT country_id FROM country WHERE LOWER(country) = ? LIMIT 1) AND transaction_date_delta = ";
-    public static final String SQL_A8_DAYS_P2 = " ) ORDER BY amount DESC LIMIT 20";
-    public static final String SQL_A15_MOST = "SELECT * FROM actor WHERE actor_id=(SELECT ar.actor_id FROM actor_rating AS ar, actor AS a WHERE rating=? AND ar.actor_id = a.actor_id AND a.gender LIKE ? GROUP BY ar.actor_id ORDER BY COUNT(ar.actor_id) DESC LIMIT 1)";
-    public static final String SQL_A15_LEAST = "SELECT * FROM actor WHERE actor_id=(SELECT ar.actor_id FROM actor_rating AS ar, actor AS a WHERE rating=? AND ar.actor_id = a.actor_id AND a.gender LIKE ? GROUP BY ar.actor_id ORDER BY COUNT(ar.actor_id) ASC LIMIT 1)";
-    public static final String SQL_A21_SOUNDTRACK = "SELECT * FROM soundtrack WHERE song=(SELECT song FROM soundtrack GROUP BY song ORDER BY COUNT(song) DESC LIMIT 1) LIMIT 1";
-    public static final String SQL_A21_MOVIE = "SELECT * FROM movie WHERE movie.movie_id IN (SELECT DISTINCT movie_id FROM most_used_song_ids)";
-    public static final String SQL_B5 = "SELECT x.release_year, y.genre FROM ( SELECT cur.* FROM movie_genre_year AS cur WHERE NOT EXISTS( SELECT high.* FROM movie_genre_year high WHERE cur.release_year = high.release_year AND high.movie_count > cur.movie_count ) ORDER BY release_year ASC ) AS x LEFT JOIN public.genre AS y ON x.genre_id = y.genre_id";
-    public static final String SQL_D1_MOST = "SELECT h.country_id, country FROM (SELECT country_id, SUM(amount) AS amount FROM public.gross cur WHERE NOT EXISTS (SELECT * FROM public.gross high WHERE high.movie_id = cur.movie_id AND high.country_id = cur.country_id AND high.amount > cur.amount) AND cur.amount IS NOT NULL AND NOT cur.country_id = 245 AND NOT cur.country_id = 247 GROUP BY cur.country_id ORDER BY amount DESC LIMIT 10) h, country c WHERE h.country_id = c.country_id";
-    public static final String SQL_D1_LEAST = "SELECT h.country_id, country FROM (SELECT country_id, SUM(amount) AS amount FROM public.gross cur WHERE NOT EXISTS (SELECT * FROM public.gross high WHERE high.movie_id = cur.movie_id AND high.country_id = cur.country_id AND high.amount > cur.amount) AND cur.amount IS NOT NULL AND NOT cur.country_id = 245 AND NOT cur.country_id = 247 GROUP BY cur.country_id ORDER BY amount ASC LIMIT 10) h, country c WHERE h.country_id = c.country_id";
-    public static final String SQL_D1_MOST_DAYS = "SELECT * FROM gross WHERE gross_id IN (SELECT gross_id FROM gross_by_period WHERE transaction_date_delta = ?) ORDER BY amount DESC LIMIT 10";
-    public static final String SQL_D1_LEAST_DAYS = "SELECT * FROM gross WHERE gross_id IN (SELECT gross_id FROM gross_by_period WHERE transaction_date_delta = ?) ORDER BY amount ASC LIMIT 10";
-    public static final String SQL_D2 = "SELECT * FROM public.movie WHERE movie_id IN (SELECT movie_id FROM public.movie_actor AS ma, public.actor AS a WHERE ma.actor_id = a.actor_id AND LOWER(a.name) = ?) ORDER BY release_year";
-    public static final String SQL_D2_REVERSE = "SELECT * FROM public.actor WHERE actor_id IN( SELECT ma.actor_id FROM public.movie_actor AS ma, public.actor AS a, public.movie AS mo WHERE ma.actor_id = a.actor_id AND ma.movie_id = mo.movie_id AND LOWER(mo.title) = ? ) ORDER BY name";
+    public static final String SQL_SELECT_MOVIE = "SELECT * " +
+            "FROM movie " +
+            "WHERE movie_id=?";
+    
+    public static final String SQL_A7_MOST = "SELECT * " +
+            "FROM movie " +
+            "WHERE budget IS NOT NULL " +
+            "ORDER BY budget DESC " +
+            "LIMIT 5";
+
+    public static final String SQL_A7_LEAST = "SELECT * " +
+            "FROM movie " +
+            "WHERE budget IS NOT NULL " +
+            "ORDER BY budget ASC " +
+            "LIMIT 5";
+
+    public static final String SQL_A8_EVER = "SELECT cur.* " +
+            "FROM gross cur " +
+            "WHERE NOT EXISTS (SELECT * " +
+                "FROM gross high " +
+                "WHERE high.movie_id = cur.movie_id " +
+                "AND high.country_id = cur.country_id " +
+                "AND high.amount > cur.amount) " +
+            "AND cur.amount IS NOT NULL " +
+            "AND cur.country_id = (SELECT country_id " +
+                "FROM country " +
+                "WHERE LOWER(country) = ? " +
+                "LIMIT 1) " +
+            "ORDER BY cur.amount DESC " +
+            "LIMIT 20";
+
+    public static final String SQL_A8_DAYS_P1 = "SELECT * " +
+            "FROM gross " +
+            "WHERE gross_id IN (SELECT gross_id " +
+                "FROM gross_by_period " +
+                "WHERE country_id = (SELECT country_id " +
+                    "FROM country " +
+                    "WHERE LOWER(country) = ? " +
+                    "LIMIT 1) " +
+                "AND transaction_date_delta = ";
+    public static final String SQL_A8_DAYS_P2 = " ) " +
+            "ORDER BY amount DESC " +
+            "LIMIT 20";
+
+    public static final String SQL_A15_MOST = "SELECT * " +
+            "FROM actor " +
+            "WHERE actor_id=(SELECT ar.actor_id " +
+                "FROM actor_rating AS ar, actor AS a " +
+                "WHERE rating=? " +
+                "AND ar.actor_id = a.actor_id " +
+                "AND a.gender LIKE ? " +
+                "GROUP BY ar.actor_id " +
+                "ORDER BY COUNT(ar.actor_id) DESC " +
+                "LIMIT 1)";
+
+    public static final String SQL_A15_LEAST = "SELECT * " +
+            "FROM actor " +
+            "WHERE actor_id=(SELECT ar.actor_id " +
+                "FROM actor_rating AS ar, actor AS a " +
+                "WHERE rating=? " +
+                "AND ar.actor_id = a.actor_id " +
+                "AND a.gender LIKE ? " +
+                "GROUP BY ar.actor_id " +
+                "ORDER BY COUNT(ar.actor_id) ASC " +
+                "LIMIT 1)";
+
+    public static final String SQL_A21_SOUNDTRACK = "SELECT * " +
+            "FROM soundtrack WHERE song=(SELECT song " +
+                "FROM soundtrack " +
+                "GROUP BY song " +
+                "ORDER BY COUNT(song) DESC LIMIT 1) " +
+            "LIMIT 1";
+
+    public static final String SQL_A21_MOVIE = "SELECT * " +
+            "FROM movie " +
+            "WHERE movie.movie_id IN (SELECT DISTINCT movie_id " +
+                "FROM most_used_song_ids)";
+
+    public static final String SQL_B5 = "SELECT x.release_year, y.genre " +
+            "FROM (SELECT cur.* " +
+                "FROM movie_genre_year AS cur " +
+                "WHERE NOT EXISTS(SELECT high.* " +
+                    "FROM movie_genre_year high " +
+                    "WHERE cur.release_year = high.release_year " +
+                    "AND high.movie_count > cur.movie_count ) " +
+                "ORDER BY release_year ASC ) AS x " +
+            "LEFT JOIN public.genre AS y " +
+            "ON x.genre_id = y.genre_id";
+
+    public static final String SQL_D1_MOST = "SELECT h.country_id, country " +
+            "FROM (SELECT country_id, SUM(amount) AS amount " +
+                "FROM public.gross cur " +
+                "WHERE NOT EXISTS (SELECT * " +
+                    "FROM public.gross high " +
+                    "WHERE high.movie_id = cur.movie_id " +
+                    "AND high.country_id = cur.country_id " +
+                    "AND high.amount > cur.amount) " +
+                "AND cur.amount IS NOT NULL " +
+                "AND NOT cur.country_id = 245 " +
+                "AND NOT cur.country_id = 247 " +
+                "GROUP BY cur.country_id " +
+                "ORDER BY amount DESC " +
+                "LIMIT 10) h, country c " +
+            "WHERE h.country_id = c.country_id";
+
+    public static final String SQL_D1_LEAST = "SELECT h.country_id, country " +
+            "FROM (SELECT country_id, SUM(amount) AS amount " +
+                "FROM public.gross cur " +
+                "WHERE NOT EXISTS (SELECT * " +
+                    "FROM public.gross high " +
+                    "WHERE high.movie_id = cur.movie_id " +
+                    "AND high.country_id = cur.country_id " +
+                    "AND high.amount > cur.amount) " +
+                "AND cur.amount IS NOT NULL " +
+                "AND NOT cur.country_id = 245 " +
+                "AND NOT cur.country_id = 247 " +
+                "GROUP BY cur.country_id " +
+                "ORDER BY amount ASC " +
+                "LIMIT 10) h, country c " +
+            "WHERE h.country_id = c.country_id";
+
+    public static final String SQL_D1_MOST_DAYS = "SELECT * " +
+            "FROM gross " +
+            "WHERE gross_id IN (SELECT gross_id " +
+                "FROM gross_by_period " +
+                "WHERE transaction_date_delta = ?) " +
+            "ORDER BY amount DESC " +
+            "LIMIT 10";
+    public static final String SQL_D1_LEAST_DAYS = "SELECT * " +
+            "FROM gross " +
+            "WHERE gross_id " +
+            "IN (SELECT gross_id " +
+                "FROM gross_by_period " +
+                "WHERE transaction_date_delta = ?) " +
+            "ORDER BY amount ASC " +
+            "LIMIT 10";
+
+    public static final String SQL_D2 = "SELECT * " +
+            "FROM public.movie " +
+            "WHERE movie_id IN (SELECT movie_id " +
+                "FROM public.movie_actor AS ma, public.actor AS a " +
+                "WHERE ma.actor_id = a.actor_id " +
+                "AND LOWER(a.name) = ?) ORDER BY release_year";
+
+    public static final String SQL_D2_REVERSE = "SELECT * " +
+            "FROM public.actor " +
+            "WHERE actor_id IN( " +
+                "SELECT ma.actor_id " +
+                "FROM public.movie_actor AS ma, public.actor AS a, public.movie AS mo " +
+                "WHERE ma.actor_id = a.actor_id " +
+                "AND ma.movie_id = mo.movie_id " +
+                "AND LOWER(mo.title) = ? ) " +
+            "ORDER BY name";
+
     public static final String SQL_B4 = "SELECT * " +
             "FROM public.country " +
             "WHERE lower(country) LIKE ?";
+
     public static final String SQL_Search_Movie = "SELECT * " +
             "FROM public.movie " +
             "WHERE LOWER(title) LIKE ?" +
